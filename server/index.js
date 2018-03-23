5d008a5faed4da508a0534b1c7e246898ae16f01
Ruslan Khusnetdinov <ruslankhh@gmail.com>
2018-03-24 01:17:10 +0300

    feat: Обновил app, server, views, добавил nodemon

---
+++ server/index.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const debug = require('debug')('yandex-shri-2018-homework-4-5-6:server');
+
+const app = require('./app');
+const normalizePort = require('./helpers/normalizePort');
+
+// Get port from environment and store in Express.
+const PORT = normalizePort(process.env.PORT || '8080');
+app.set('port', PORT);
+
+// Create HTTP server.
+const server = http.createServer(app);
+
+// Listen on provided port, on all network interfaces.
+server.listen(PORT, () => {
+  console.log(`Express app listening on port ${PORT}!`);
+});
+
+server.on('error', (error) => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  const bind = typeof PORT === 'string'
+    ? 'Pipe ' + PORT
+    : 'Port ' + PORT;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+    default:
+      throw error;
+  }
+});
+
+server.on('listening', () => {
+  const addr = server.address();
+  const bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+});
