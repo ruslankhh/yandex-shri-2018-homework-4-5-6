1e34d83f7e6d7dc9a1333392fabc98d03fcdf2d6
Ruslan Khusnetdinov <ruslankhh@gmail.com>
2018-03-24 23:22:27 +0300

    feat: Сделал вывод списка вайлов на главной странице

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
+
+const git = require('./../helpers/git');
+const parseFileList = require('./../helpers/parseFileList');
+
+const config = require('./../../app.json');
+
 const router = express.Router();
 
 // GET home page.
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  const title = config.menu && config.menu.length !== 0 ? config.menu : '';
+  const branch = 'master';
+  const pathname = '';
+  const lsPath = pathname || '.';
+  const cwd = config.repositoryDiractory;
+
+  git(`ls-tree -r -t ${branch} ${lsPath}`, { cwd })
+    .then(data => {
+      const files = parseFileList(data).filter(file => pathname === file.dir);
+      const tree = { children: files };
+
+      res.render('index', { title, branch, tree });
+    });
 });
 
 module.exports = router;
