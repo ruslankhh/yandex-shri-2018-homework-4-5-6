94a3a06a4a99315425507ed326be7f4389d3f287
Ruslan Khusnetdinov <ruslankhh@gmail.com>
2018-03-25 15:15:21 +0300

    fix: Поправил переключение веток на главной странице

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,13 +1,21 @@
 const objectSelect = document.getElementsByClassName('branch__select')[0];
+const oldObject = objectSelect.value;
 
 if (objectSelect) {
   objectSelect.addEventListener('change', (e) => {
     const newObject = objectSelect.value;
     const { pathname } = window.location;
-    const [, type, object, filepath = ''] = pathname.match(/^\/(\w+)\/?(\w+)\/?(.*?)?$/);
+    const pathnameMatch = pathname
+      ? pathname.match(/^\/(\w+)\/?(\w+)\/?(.*?)?$/)
+      : null;
+    const pathnameArr = pathnameMatch
+      ? pathnameMatch.slice(1)
+      : ['tree', oldObject];
+    const [type, object, filepath] = pathnameArr;
 
     if (newObject !== object) {
-      window.location.pathname = `/${type}/${newObject}/${filepath}`;
+      const newPathnameArr = [type, newObject, filepath].filter(s => !!s);
+      window.location.pathname = `/${newPathnameArr.join('/')}`;
     }
   });
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,9 @@
+const _ = require('lodash');
 const path = require('path');
 const express = require('express');
 const git = require('./../helpers/git');
 const parseFileList = require('./../helpers/parseFileList');
+const parseBranchList = require('./../helpers/parseBranchList');
 const config = require('./../../app.json');
 
 const router = express.Router();
@@ -27,7 +29,7 @@ router.get('/', (req, res, next) => {
       const root = { filepath: '', type: 'tree', base: config.name, level: -1 };
       const files = parseFileList(data[0]);
       const breadcrumbs = [root];
-      const branches = [branch];
+      const branches = _.uniq([branch, ...parseBranchList(data[1])]);
       const children = files.filter(file => pathname === file.dir);
       const tree = { children };
 
