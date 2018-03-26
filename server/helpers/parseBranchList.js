78f7dbcc303bd2c4be10fb333a3c3a45b10dc2f4
Ruslan Khusnetdinov <ruslankhh@gmail.com>
2018-03-26 09:48:36 +0300

    fix: Поправил парсинг списка веток

--- server/helpers/parseBranchList.js
+++ server/helpers/parseBranchList.js
@@ -1,8 +1,6 @@
 const parseBranchList = (value) => {
   const branches = value.toString().trim().split(/\n/g)
-    .map(item => { console.log(item); return item; })
-    .map(branch => branch.replace('* ', '').replace('/', '--'))
-    .map(item => { console.log(item); return item; })
+    .map(branch => branch.trim().replace('* ', '').replace('/', '--'))
     .sort();
 
   return branches;
