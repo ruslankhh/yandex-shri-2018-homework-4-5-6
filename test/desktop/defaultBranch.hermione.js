3dc330ecd212c4a68196897b2620786768c869b9
Ruslan Khusnetdinov <ruslankhh@gmail.com>
2018-03-31 20:58:57 +0300

    test: Сделал интеграционные тесты для default branch

--- test/desktop/defaultBranch.hermione.js
+++ test/desktop/defaultBranch.hermione.js
@@ -1,47 +1,57 @@
+const _ = require('lodash');
 const { expect } = require('chai');
 
-const cleanMockRepo = require('./../utils/cleanMockRepo');
-const createMockRepo = require('./../utils/createMockRepo');
-const growMockRepo = require('./../utils/growMockRepo');
-const config = require('./../../config.json');
+const config = { ...require('./../../config.json'), ...require('./../data/data.json') };
+
+const { commits } = config;
+const defaultBranch = config.defaultBranch || 'master';
 
 describe('default branch', () => {
-  const cwd = config.repoDir;
-  const commits = [
-    {
-      master: [{ filepath: 'README.md', content: '# Hello, world!' }],
-      feature: [{ filepath: 'main.js', content: "console.log('It\\'s work');" }],
-      test: [{ filepath: 'test.test.js', content: "console.log('Test!');" }]
-    },
-    {
-      test: [{ filepath: 'test2.test.js', content: "console.log('Test!');" }]
-    },
-    {
-      test: [
-        { filepath: 'test.test.js', content: "console.log('Test1!');" },
-        { filepath: 'test2.test.js', content: "console.log('Test2!');" },
-        { filepath: 'test3.test.js', content: "console.log('Test3!');" }
-      ]
-    }
-  ];
-
-  beforeEach(() =>
-    Promise.resolve()
-      .then(() => cleanMockRepo(cwd))
-      .then(() => createMockRepo(cwd, commits[0]))
-      .then(() => growMockRepo(cwd, commits[1]))
-      .then(() => growMockRepo(cwd, commits[2]))
-  );
-
   // eslint-disable-next-line
   it('из списка всех веток отображается ветка по умолчанию', function () {
-    const expected = 'master';
+    const expected = defaultBranch;
 
     return this.browser
       .url('/')
       .then(() => this.browser.getHTML('.branch-select', false))
       .then(data => {
         expect(data).to.contain(expected);
+      });
+  });
+
+  // eslint-disable-next-line
+  it('для ветки отображается список коммитов', function () {
+    const expected = commits.filter(commit => !!commit[defaultBranch]).length;
+
+    return this.browser
+      .url('/')
+      .then(() =>
+        this.browser.element('.header__nav').click('.nav-item__link[href^="/commits"]')
+      )
+      .then(() => this.browser.elements('.commit-item', false))
+      .then(data => {
+        expect(data.value.length).to.equal(expected);
+      });
+  });
+
+  // eslint-disable-next-line
+  it('для ветки отображается корректный список файлов и папок', function () {
+    const expected = _.uniq(
+      _.flatten(
+        commits
+          .filter(commit => !!commit[defaultBranch])
+          .map(commit => commit[defaultBranch].map(file => file.filepath))
+      )
+    ).length;
+
+    return this.browser
+      .url('/')
+      .then(() =>
+        this.browser.element('.header__nav').click('.nav-item__link[href^="/tree"]')
+      )
+      .then(() => this.browser.elements('.file-item', false))
+      .then(data => {
+        expect(data.value.length).to.equal(expected);
       });
   });
 });
